bb47f4bbb3506b8f247473399b42bb4f2613fe03
Administrator <gys646686419dqq@163.com>
2018-12-06 11:42:39 +0800

    Default Changelist

--- src/main/java/com/gys/fulixcx/controller/AppController.java
+++ src/main/java/com/gys/fulixcx/controller/AppController.java
@@ -104,7 +104,13 @@ public class AppController {
     @Autowired
     CallCompanyDao callCompanyDao;
     @GetMapping("/registerCom")
-    public JsonReq registerCom(String ComName,String corporation,String manage,String phone){
+    public JsonReq registerCom(HttpServletRequest request,String ComName,String manage,String phone,String code,String pass){
+        String code1 = (String) request.getSession().getServletContext().getAttribute(phone);
+        if (!code.equals(code1)){
+            return new JsonReq(201,"验证码错误");
+        }else {
+            request.getSession().getServletContext().removeAttribute(phone);
+        }
         CallStaffMode byStaffPhone = callStaffDao.findByStaffPhone(phone);
         if (byStaffPhone != null){
             return new JsonReq(201,"账号已注册");
@@ -116,7 +122,7 @@ public class AppController {
         }
         CallCompanyMode companyMode = new CallCompanyMode();
         companyMode.setCompanyName(ComName);
-        companyMode.setCompanyCorporation(corporation);
+        companyMode.setCompanyCorporation("");
         companyMode.setCompanyManage(manage);
         companyMode.setCreatTime(new Date().getTime()+"");
         companyMode.setEndTime((new Date().getTime()+3*24*60*60*1000)+"");
@@ -126,8 +132,8 @@ public class AppController {
         byStaffPhone.setCompanyId(save.getId());
         byStaffPhone.setStaffName(manage);
         byStaffPhone.setStaffPhone(phone);
-        byStaffPhone.setPassWord(MD5Util.StringToMd5(phone.substring(phone.length()-4,phone.length())));
-        byStaffPhone.setState(0);
+        byStaffPhone.setPassWord(MD5Util.StringToMd5(pass));
+        byStaffPhone.setState(1);
         byStaffPhone.setStaffManage(2);
         byStaffPhone.setCreatTime(""+new Date().getTime());
         callStaffDao.save(byStaffPhone);
@@ -525,19 +531,22 @@ public class AppController {
         System.out.println("]’‘’‘’‘’" );
         return new Msn(0,"接收成功");
     }
-    @GetMapping("Msm")
-    public void n(){
+    @GetMapping("MsmCode")
+    public JsonReq MsmCode(HttpServletRequest req,String phone){
+        System.out.println(phone);
+        Random r = new Random();
+        int code = r.nextInt(8999)+1000;
         try {
         //设置超时时间-可自行调整
         System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
         System.setProperty("sun.net.client.defaultReadTimeout", "10000");
-//初始化ascClient需要的几个参数
+        //初始化ascClient需要的几个参数
         final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
         final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
-//替换成你的AK
+        //替换成你的AK
         final String accessKeyId = "LTAIvIJefMjU4Z6m";//你的accessKeyId,参考本文档步骤2
         final String accessKeySecret = "cXKIKGoCTaquF2cBmbViIOv9N6wq5O";//你的accessKeySecret，参考本文档步骤2
-//初始化ascClient,暂时不支持多region（请勿修改）
+        //初始化ascClient,暂时不支持多region（请勿修改）
         IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
                 accessKeySecret);
         DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
@@ -547,28 +556,34 @@ public class AppController {
         //使用post提交
         request.setMethod(MethodType.POST);
         //必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式；发送国际/港澳台消息时，接收号码格式为国际区号+号码，如“85200000000”
-        request.setPhoneNumbers("13527586651");
+        request.setPhoneNumbers(phone);
         //必填:短信签名-可在短信控制台中找到
         request.setSignName("拓客Talk");
         //必填:短信模板-可在短信控制台中找到，发送国际/港澳台消息时，请使用国际/港澳台短信模版
         request.setTemplateCode("SMS_150940078");
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
         //友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
-        request.setTemplateParam("{\"code\":\"1234\"}");
+        request.setTemplateParam("{\"code\":\""+code+"\"}");
         //可选-上行短信扩展码(扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段)
         //request.setSmsUpExtendCode("90997");
         //可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
         request.setOutId("yourOutId");
-//请求失败这里会抛ClientException异常
+        //请求失败这里会抛ClientException异常
         SendSmsResponse sendSmsResponse = null;
 
             sendSmsResponse = acsClient.getAcsResponse(request);
 
         if(sendSmsResponse.getCode() != null && sendSmsResponse.getCode().equals("OK")) {
-//请求成功
+            //请求成功
+            req.getSession().getServletContext().setAttribute(phone,""+code);
+            return new JsonReq("验证码发送成功");
+
+        }else {
+            return new JsonReq(201,"发送验证码失败");
         }
         } catch (ClientException e) {
             e.printStackTrace();
+            return new JsonReq(201,"发送验证码失败");
         }
     }
 
